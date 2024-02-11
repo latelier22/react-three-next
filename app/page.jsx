9eeb5c8d77296ab293d9e78c036eeb6f1dc5600e
Cyrille <latelier.cyrille@gmail.com>
2024-02-11 11:19:48 +0100

    Texte Mug

--- app/page.jsx
+++ app/page.jsx
@@ -74,7 +74,7 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
         {/* first row */}
         <div className='relative h-72 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'> {/* Ajustement de la hauteur ici */}
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
+          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>MUGd</h2>
           <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
         </div>
         <div className='relative my-12 h-72 w-full py-6 sm:w-1/2 md:mb-40'> {/* Ajustement de la hauteur ici */}
@@ -86,27 +86,6 @@ export default function Page() {
           </View>
         </div>
         {/* second row */}
-        <div className='relative my-12 h-72 w-full py-6 sm:w-1/2 md:mb-40'> {/* Ajustement de la hauteur ici */}
-          <View orbit className='relative h-full animate-bounce sm:h-48 sm:w-full'>
-            <Suspense fallback={null}>
-              <Duck route='/blob' scale={2} position={[0, -1.6, 0]} />
-              <Common color={'lightblue'} />
-            </Suspense>
-          </View>
-        </div>
-
-
-
-
-        <div className='w-full p-6 sm:w-1/2'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h2>
-          <p className='mb-8 text-gray-600'>
-            3D Divs are renderer through the View component. It uses gl.scissor to cut the viewport into segments. You
-            tie a view to a tracking div which then controls the position and bounds of the viewport. This allows you to
-            have multiple views with a single, performant canvas. These views will follow their tracking elements,
-            scroll along, resize, etc.
-          </p>
-        </div>
       </div>
     </>
   )
